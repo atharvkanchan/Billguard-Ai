f9d5938cc99eebed3a4cfb0cf3d19ebb13e97a4d
atharvkanchan <kanchanatharvl614@gmail.com>
2026-03-16 13:21:32 +0530

    Create bill_parser.py

---
+++ bill_parser.py
@@ -0,0 +1,19 @@
+import re
+
+def parse_bill(text):
+
+    data = {}
+
+    gst_pattern = r'[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z][A-Z0-9]Z[A-Z0-9]'
+    invoice_pattern = r'INV[0-9]+'
+    date_pattern = r'\d{2}/\d{2}/\d{4}'
+
+    gst = re.findall(gst_pattern,text)
+    invoice = re.findall(invoice_pattern,text)
+    date = re.findall(date_pattern,text)
+
+    data["gst"] = gst[0] if gst else None
+    data["invoice"] = invoice[0] if invoice else None
+    data["date"] = date[0] if date else None
+
+    return data
