34bb36972dc4e461f7514a72ddcde6a38130ec04
atharvkanchan <kanchanatharvl614@gmail.com>
2026-03-16 13:18:07 +0530

    Update ocr_utils.py

--- ocr_utils.py
+++ ocr_utils.py
@@ -1,10 +1,17 @@
 import pytesseract
+import cv2
+import numpy as np
 from PIL import Image
 
 def extract_text(image):
 
     img = Image.open(image)
+    img = np.array(img)
 
-    text = pytesseract.image_to_string(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    thresh = cv2.threshold(gray,150,255,cv2.THRESH_BINARY)[1]
+
+    text = pytesseract.image_to_string(thresh)
 
     return text
