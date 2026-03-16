13bd3a9fe7c701ebe137ef22a3288999a516bf13
atharvkanchan <kanchanatharvl614@gmail.com>
2026-03-16 13:24:29 +0530

    Update gst_validator.py

--- gst_validator.py
+++ gst_validator.py
@@ -4,4 +4,7 @@ def validate_gstin(gstin):
 
     pattern = r'^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z][A-Z0-9]Z[A-Z0-9]$'
 
-    return bool(re.match(pattern,gstin))
+    if gstin and re.match(pattern,gstin):
+        return True
+
+    return False
